c8060e595729dbd2304c8bfb809df58bb75dcf81
Akul Kapoor <akulkapoor@gmail.com>
2012-10-02 17:08:49 -0400

    First one

---
+++ BackBeat/app2.js
@@ -0,0 +1,69 @@
+var data1;
+var data2;
+onReady = function() {
+		}
+
+		doSearch = function() {
+			$.getJSON('http://ws.audioscrobbler.com/2.0/',
+			{
+				method: "artist.getSimilar",
+				api_key: "8319d81dde2f49bad5c65a0ce2361a31",
+				format: "json",
+				artist: $("#artistSearch").val(),
+				limit: 1000
+			},
+
+			function(data) {
+				data1 = data;
+				$.getJSON('http://ws.audioscrobbler.com/2.0/',
+				{
+					method: "tag.getTopArtists",
+					api_key: "8319d81dde2f49bad5c65a0ce2361a31",
+					format: "json",
+					tag: $("#locationSearch").val(),
+					limit: 9000
+				},
+
+				function(data) {
+				data2 = data;
+				$("#results").html("");
+				var names = [];
+				$.each(data2.topartists.artist, function(i, item) {
+					names.push(item.name);
+				});	
+
+
+
+
+				$.each(data1.similarartists.artist, function(i, item) {
+				//alert($.inArray(item,data2.topartists.artist));	
+					if ($.inArray(item.name,names) !== -1) {
+						var artist = document.createElement("div");
+						artist.className = "artist";
+						artist.id = item.name;
+
+						var img = document.createElement("div");
+						img.className = "img";
+						img.innerHTML = "<img src=" + item.image[3]["#text"] + ">"
+
+						var link = document.createElement("div");
+						link.className = "link";
+						if (item.url.slice(0,7) !== "http://") {
+							link.innerHTML = "<a href='" + "http://" + item.url + "'>" + item.name + "</a>";
+						}
+						else {
+							link.innerHTML = "<a href='" + item.url + "'>" + item.name + "</a>";
+						}
+						artist.appendChild(link);
+						artist.appendChild(img);
+						artist.innerHTML += "<br>"
+						$("#results").append(artist);
+					}
+				});
+			});
+		});
+	}
+
+
+
+$(document).ready(onReady);
